80e039b5b4e210293d9247468d32d693a01f9abd
Genki Takiuchi <genki@s21g.com>
2023-03-12 07:35:46 +0900

    Setup example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,33 @@
-import { component$ } from '@builder.io/qwik';
+import {
+  component$,
+} from '@builder.io/qwik';
+import {
+  globalAction$,
+} from '@builder.io/qwik-city';
+
+export const Foo = globalAction$(() => {
+  console.log('foo');
+});
+
+export const Bar = globalAction$(() => {
+  console.log('bar');
+});
 
 export default component$(() => {
+  const foo = Foo.use();
+  const bar = Bar.use();
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div>
+      <button onClick$={() => {
+        foo.run();
+      }}>Foo</button>
+      <button onClick$={() => {
+        bar.run();
+      }}>Bar</button>
+      <button onClick$={() => {
+        foo.run();
+        bar.run();
+      }}>Foo Bar</button>
+    </div>
   );
 });

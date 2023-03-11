3c1319eecdb072189a709bb18020e56ddc6ed878
Genki Takiuchi <genki@s21g.com>
2023-03-12 07:27:04 +0900

    Clean up and added Makefile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,9 @@
-import { component$, useClientEffect$ } from '@builder.io/qwik';
-import { loader$ } from '@builder.io/qwik-city';
-
-export const Loader = loader$(({cookie}) => {
-  cookie.set('theme', 'dark', {path: '/', maxAge: 60 * 60 * 24 * 365});
-  console.log("loader", cookie.headers());
-  return "loader";
-});
-
-export const onGet = (({cookie}) => {
-  cookie.set('theme', 'light', {path: '/', maxAge: 60 * 60 * 24 * 365});
-  console.log("onGet", cookie.headers());
-});
+import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
-  const loader = Loader.use();
-  console.log(loader.value);
-  useClientEffect$(() => {
-    const pre = document.querySelector('pre');
-    pre.textContent = JSON.stringify(document.cookie, null, 2);
-  });
   return (
     <>
-      <div>Cookie should go here:</div>
-      <pre />
+      <div>hello</div>
     </>
   );
 });

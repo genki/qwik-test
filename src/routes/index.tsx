332697bed758b02419e7324a465b4f353b829ca0
Genki Takiuchi <genki@s21g.com>
2023-08-24 08:09:03 +0900

    Testing useOnWindow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,28 @@
-import { component$, useStyles$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {
+  component$, useStyles$, useOnWindow, $,
+  useVisibleTask$,
+} from "@builder.io/qwik";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 import styles from "./styles.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
+  useOnWindow("beforeunload", $((ev) => {
+    ev.preventDefault();
+    ev.returnValue = "custom message";
+  }));
+  /* this works.
+  useVisibleTask$(({track}) => {
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
+      ev.returnValue = "custom message";
+    });
+  }); */
   return (
     <>
       <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-      <footer>
-        <ul>
-        <li><Link href="/">Top</Link></li>
-        <li><Link href="/home">Home</Link></li>
-        </ul>
-      </footer>
+      <p>Test</p>
     </>
   );
 });

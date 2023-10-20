28109526ce1445e0d681f2f2a5ff110a56a066d3
Genki Takiuchi <genki@s21g.com>
2023-10-21 01:46:04 +0900

    Example for bad serialization

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,11 @@ import { server$ } from "@builder.io/qwik-city";
 import styles from "./styles.css?inline";
 
 const binstr = '\x18;\x13,ëÆ¹L\x0F\x97¥y\x90çRÂ«8âc;E\x19©vÚõ\x96üx*\x92';
+const udstr1 = '\u0001';
+const udstr2 = '\u0001abcd';
 
 export const getFoo = server$(async function (){
-  return {foo:"foo", bar:binstr};
+  return {foo:"foo", bar:binstr, udstr1, udstr2};
 });
 
 export default component$(() => {
@@ -16,6 +18,7 @@ export default component$(() => {
       <h1>Hi 👋</h1>
       <p>foo: {foo.value}</p>
       <button onClick$={async () => {
+        console.log("clicked", await getFoo());
         foo.value = JSON.stringify(await getFoo());
       }}>Click me</button>
     </>
